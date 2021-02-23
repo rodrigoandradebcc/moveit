148798ef129ca7d7ee812ef3c837fa6b0f5c4ac4
Rodrigo Andrade <randrade@brbatel.com.br>
2021-02-23 00:41:13 -0300

    Dia 01 NLW

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ExperienceBar from './components/ExperienceBar';
+import GlobalStyles from './styles/GlobalStyles';
+import { Container } from './styles/styles';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>NLW</h1>
+      <ExperienceBar />
+      <GlobalStyles />
+    </Container>
   );
-}
+};
 
 export default App;
